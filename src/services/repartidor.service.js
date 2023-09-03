6d2a350ccbc2ff0284fea7b0e5c7e8601bd75f1c
Salocin0 <nicolasarieldurelli@gmail.com>
2023-09-03 20:02:37 -0300

    fix

--- src/services/repartidor.service.js
+++ src/services/repartidor.service.js
@@ -1,53 +1,42 @@
-import { Repartidor } from "../DAO/models/repartidor.model.js";
+import { Repartidor } from '../DAO/models/repartidor.model.js';
 
 class RepartidorService {
   //ACTUALIZAR METODOS CON LOS NUEVOS ATRIBUTOS DE REPARTIDOR
-    async getAll(){
-      const repartidores = await Repartidor.findAll(); 
-      return repartidores
+  async getAll() {
+    const repartidores = await Repartidor.findAll();
+    return repartidores;
+  }
+
+  async getOne(id) {
+    const repartidor = Repartidor.findByPk(id);
+    return repartidor;
+  }
+
+  // Atributos del repartidor
+  async updateOne(id, repartidorData) {
+    const repartidor = await Repartidor.findByPk(id);
+
+    if (repartidor) {
+      repartidor.razonSocial = repartidorData.razonSocial;
+      repartidor.cuit = repartidorData.cuit;
+
+      await repartidor.save();
+
+      return repartidor;
+    } else {
+      console.log('No lo encontre');
+      return null;
     }
-
-    async getOne(id) {
-        const repartidor = Repartidor.findByPk(id);
-        return repartidor;
-      }
-  
-      // Atributos del repartidor
-      async updateOne(id, repartidorData) {
-        const repartidor = await Repartidor.findByPk(id);
-
-        if (repartidor) {
-          repartidor.razonSocial = repartidorData.razonSocial;
-          repartidor.cuit = repartidorData.cuit;
-
-          await repartidor.save();
-
-          return repartidor;
-        } else {
-          console.log("No lo encontre")
-          return null;
-        }
-      }
-    async create(nuevorepartidor) {
-        const repartidorendb = await Repartidor.findOne({
-          where: {
-            // Algun atributo del repartidor
-            cuit: nuevorepartidor.cuit,
-          },
-        });
-        if (repartidorendb) {
-          return false;
-        } else {
-          const repartidorCreado = await Repartidor.create(nuevorepartidor);
-          return repartidorCreado;
-        }
-    }
-    async delete(id) {
-        const repartidor = await Repartidor.findByPk(id);
-        repartidor.habilitado = false;
-        await repartidor.save();
-    }
-
+  }
+  async create(nuevorepartidor) {
+    const repartidorCreado = await Repartidor.create(nuevorepartidor);
+    return repartidorCreado;
+  }
+  async delete(id) {
+    const repartidor = await Repartidor.findByPk(id);
+    repartidor.habilitado = false;
+    await repartidor.save();
+  }
 }
 
 export const repartidorService = new RepartidorService();
