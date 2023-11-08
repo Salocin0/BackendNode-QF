21e5797813ed567c9ec0c97279380ef2badc45bd
Ignacio Sachetto <nachosachetto1998@hotmail.com>
2023-11-08 18:54:10 -0300

    enPreparacion done

--- src/estados/estadosConcretosEventos/enPreparacionEvento.state.js
+++ src/estados/estadosConcretosEventos/enPreparacionEvento.state.js
@@ -1,17 +1,31 @@
 export const enPreparacion = {
-    crearEvento: async (evento) => {
-      evento.estado = 'enPreparacion';
+  crearEvento: async (evento) => {
+      evento.estado = 'En Preparación';
       await evento.save();
       return evento;
-    },
+  },
 
-    confirmar: async (evento) => {
-      evento.estado = 'confirmar';
+  confirmarEvento: async (evento) => {
+      evento.estado = 'Confirmado';
       await evento.save();
       return evento;
-    }
+  },
 
+  cancelarEvento: async (evento) => {
+      evento.estado = 'Cancelado';
+      await evento.save();
+      return evento;
+  },
 
+  iniciarEvento: async (evento) => {
+      throw new Error('No se puede iniciar un evento directamente desde "En Preparación"');
+  },
 
+  finalizarEvento: async (evento) => {
+      throw new Error('No se puede finalizar un evento directamente desde "En Preparación"');
+  },
 
-  };
+  reprogramarEvento: async (evento) => {
+      throw new Error('No se puede reprogramar un evento directamente desde "En Preparación"');
+  },
+};
