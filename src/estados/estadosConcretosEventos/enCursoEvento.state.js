393c4c5c898fb7da227ecca71ae10c6120d9616e
Ignacio Sachetto <nachosachetto1998@hotmail.com>
2023-11-08 20:12:55 -0300

    enCursoEvento implemented

--- src/estados/estadosConcretosEventos/enCursoEvento.state.js
+++ src/estados/estadosConcretosEventos/enCursoEvento.state.js
@@ -1,17 +1,27 @@
 export const enCurso = {
-    crearEvento: async (evento) => {
-      evento.estado = 'enPreparacion';
-      await evento.save();
-      return evento;
-    },
+  crearEvento: async (evento) => {
+    throw new Error('No se puede crear un evento que ya esta en curso.');
+  },
 
-    confirmar: async (evento) => {
-      evento.estado = 'confirmar';
-      await evento.save();
-      return evento;
-    }
+  confirmarEvento: async (evento) => {
+    throw new Error('El evento ya esta confirmado.');
+  },
 
+  cancelarEvento: async (evento) => {
+    throw new Error('No se puede cancelar un evento que está en curso.');
 
+  },
 
+  iniciarEvento: async (evento) => {
+      throw new Error('No se puede iniciar un evento que ya se encuentra en curso');
+  },
 
-  };
+  finalizarEvento: async (evento) => {
+    evento.estado = 'Finalizado';
+    await evento.save();
+    return evento;  },
+
+  reprogramarEvento: async (evento) => {
+      throw new Error('No se puede reprogramar un evento que está en curso');
+  },
+};
