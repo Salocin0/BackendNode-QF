6d42b4482d0c90201420f32cfda9bb64e5fbaac1
Salocin0 <nicolasarieldurelli@gmail.com>
2024-01-09 17:26:34 -0300

    fix app.js 2

--- src/app.js
+++ src/app.js
@@ -24,10 +24,8 @@ import { RouterPuesto } from './routes/puesto.router.js';
 import { RouterRepartidor } from './routes/repartidor.router.js';
 import { RouterRestriccion } from './routes/restriccion.router.js';
 import { RouterUser } from './routes/user.router.js';
-
 import { procesosAutomaticos } from './util/procesosAutomaticos.js';
 import * as dotenv from 'dotenv'
-
 dotenv.config();
 import { sequelize } from './util/connections.js';
 //definicion de server de express
