acb6fa23c787f553056858853ea99f98e1237c58
Salocin0 <nicolasarieldurelli@gmail.com>
2024-01-12 20:43:10 -0300

    .js

--- src/util/connections.js
+++ src/util/connections.js
@@ -1,6 +1,6 @@
 import 'dotenv/config';
 import { Sequelize } from 'sequelize';
-import { Productor } from '../DAO/models/productor.model';
+import { Productor } from '../DAO/models/productor.model.js';
 
 export const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
   host: process.env.DB_HOST,
