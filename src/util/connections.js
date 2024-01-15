a8bae18d42e65faf710ae9d1dc4e49737e819eb2
Salocin0 <nicolasarieldurelli@gmail.com>
2024-01-15 14:22:49 -0300

    fix connections

--- src/util/connections.js
+++ src/util/connections.js
@@ -4,5 +4,7 @@ import { Sequelize } from 'sequelize';
 export const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
   host: process.env.DB_HOST,
   dialect: process.env.DB_DIALECT,
-  ssl: process.env.DB_SSL
+  dialectOptions: {
+    ssl:process.env.DB_SSL === 'true',
+  },
 });
