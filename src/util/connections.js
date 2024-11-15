7bafc9d579a398135c7df8cd08223a240f5311f2
Salocin0 <nicolasarieldurelli@gmail.com>
2024-11-15 14:33:07 -0300

    a

--- src/util/connections.js
+++ src/util/connections.js
@@ -12,7 +12,7 @@ export const sequelize = new Sequelize({
   schema: process.env.DB_SCHEMA,
   dialect: process.env.DB_DIALECT || 'postgres',
   dialectOptions: {
-    ssl: process.env.DB_SSL === 'true' ? { 
+    ssl: process.env.DB_SSL == 'true' ? { 
       require: true, 
       rejectUnauthorized: false 
     } : false,
