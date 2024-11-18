9e279ceeee728c03180f0654e6367bb76c109922
Salocin0 <nicolasarieldurelli@gmail.com>
2024-11-18 00:42:54 -0300

    service nuevo

--- src/util/firebase-admin.js
+++ src/util/firebase-admin.js
@@ -1,6 +1,6 @@
 // firebase-admin.js
 import admin from 'firebase-admin'; // Ajusta la importación según tu configuración de Firebase Admin
-import serviceAccount from "../../serviceAccountKey.json" assert { type: "json" };
+import serviceAccount from "../../etc/secrets/serviceAccountKey.json" assert { type: "json" };
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
