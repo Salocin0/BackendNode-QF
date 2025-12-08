78aae07428af96a8b3e72792866a2ccd13df77e1
Salocin0 <nicolasarieldurelli@gmail.com>
2025-12-08 15:46:11 -0300

    cors mejorados

--- src/app.js
+++ src/app.js
@@ -85,6 +85,19 @@ app.options("*", (req, res) => {
   res.sendStatus(204);
 });
 
+// Fallback CORS middleware: asegura cabeceras en TODAS las respuestas (incluyendo errores)
+// Esto protege contra proxies/hosting que puedan eliminar cabeceras en respuestas de error.
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,PATCH,DELETE,OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+  res.header('Access-Control-Allow-Credentials', 'true');
+  if (req.method === 'OPTIONS') {
+    return res.sendStatus(204);
+  }
+  next();
+});
+
 app.use(morgan('dev'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
