b6c11d65bbb3f4c8ce143f29cc9c05e32910dd1a
Salocin0 <nicolasarieldurelli@gmail.com>
2026-02-04 19:52:18 -0300

    fix peticion eventos

--- src/app.js
+++ src/app.js
@@ -74,7 +74,7 @@ app.use(
   cors({
     origin: true, // refleja el Origin de la petición en Access-Control-Allow-Origin
     methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization', 'Origin', 'X-Requested-With', 'Accept'],
+    allowedHeaders: ['Content-Type', 'Authorization', 'Origin', 'X-Requested-With', 'Accept', 'consumidorid'],
     credentials: true,
   })
 );
@@ -87,7 +87,7 @@ app.options("*", cors());
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,PATCH,DELETE,OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization, consumidorid');
   res.header('Access-Control-Allow-Credentials', 'true');
   if (req.method === 'OPTIONS') {
     return res.sendStatus(204);
